d3da76f2e0f67341dbc777cadd568108ac5bbbe2
Gabriel <olaniyi_g@hotmail.com>
2020-01-26 22:29:53 +0100

    localstorage persistence with redux-persist

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/redux/user/user.selectors.js
+++ src/redux/user/user.selectors.js
@@ -4,5 +4,5 @@ const selectUser = state => state.user;
 
 export const selectCurrentUser = createSelector(
   [selectUser],
-  user => user.selectUser
+  user => user.currentUser
 );
